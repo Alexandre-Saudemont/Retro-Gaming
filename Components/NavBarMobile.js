489863ea75fbb8d11dca3166388c11bf4faaa394
Alexandre S <xetoxin@gmail.com>
2024-07-11 19:36:38 +0200

    adding search bar in mobile and hidding navbar in desktop mode

--- Components/NavBarMobile.js
+++ Components/NavBarMobile.js
@@ -1,25 +1,39 @@
+import {useState} from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from '../styles/NavBarMobile.module.scss';
 
 export default function NavBarMobile({children}) {
+	const [showSearch, setShowSearch] = useState(false);
+
+	const handleSearchClick = () => {
+		setShowSearch(!showSearch);
+	};
+
 	return (
-		<div className={styles.NavBarMobile}>
+		<div className={styles.NavBarMobileWrapper}>
 			{children}
-			<nav className={styles.showMenu}>
-				<Link href='/'>
-					<Image src='/icon-home-gameboy.svg' alt='home icon' width={50} height={50} />
-				</Link>
-				<Link href='/Register'>
-					<Image src='/user-register2.svg' alt='icon register' width={60} height={60} />
-				</Link>
-				<Link href='/Search'>
-					<Image src='/search-icon.svg' alt='icon search' width={50} height={50} />
-				</Link>
-				<Link href='/About'>
-					<Image src='/about-icon.svg' alt='icon about' width={50} height={50} />
-				</Link>
-			</nav>
+			<div className={styles.NavBarMobile}>
+				{showSearch && (
+					<div className={styles.searchBar}>
+						<input type='text' placeholder='Rechercher...' />
+					</div>
+				)}
+				<nav className={styles.showMenu}>
+					<Link href='/'>
+						<Image src='/icon-home-gameboy.svg' alt='home icon' width={50} height={50} />
+					</Link>
+					<Link href='/Register'>
+						<Image src='/user-register2.svg' alt='icon register' width={60} height={60} />
+					</Link>
+					<div onClick={handleSearchClick} className={styles.searchIcon}>
+						<Image src='/search-icon.svg' alt='icon search' width={50} height={50} />
+					</div>
+					<Link href='/About'>
+						<Image src='/about-icon.svg' alt='icon about' width={50} height={50} />
+					</Link>
+				</nav>
+			</div>
 		</div>
 	);
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,13 @@ import '../styles/Register.scss';
 import '../styles/HomePage.module.scss';
 import '../styles/Globals.scss';
 import '../styles/Fonts.scss';
-import NavBarMobile from '../components/NavBarMobile';
+import NavBarMobile from '../Components/NavBarMobile';
 
 export default function App({Component, pageProps}) {
 	return (
-		<NavBarMobile>
+		<>
 			<Component {...pageProps} />;
-		</NavBarMobile>
+			<NavBarMobile />
+		</>
 	);
 }
